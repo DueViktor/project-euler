d0e2eec852d7235cf79f86db1d153f4a239b291a
DueViktor <viktorduep@gmail.com>
2021-02-20 20:59:47 +0100

    renaming of folder

--- problems/001.py
+++ problems/001.py
@@ -3,9 +3,9 @@ start = time.time()
 
 sum = 0
 
-for i in range(1,1000):
+for i in range(1, 1000):
     if i % 3 == 0 or i % 5 == 0:
         sum += i
-    
+
 print("The sum of all multiples of 3 or 5 below 1000 is {}".format(sum))
-print("Time: {}".format(time.time()-start))
+print("Time: {}".format(time.time() - start))
